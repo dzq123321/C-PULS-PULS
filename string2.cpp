aba2e70db82cd1e543eee4d7ee8da0af4af436a4
dzq123321 <2625869004@qq.com>
2020-02-19 21:00:30 +0800

    1

--- string2.cpp
+++ string2.cpp
@@ -1,3 +1,4 @@
+#if 1
 #include<iostream>
 #include<string>
 #include<assert.h>
@@ -88,7 +89,7 @@ using namespace std;
 //	cout << s3 << endl;
 //}
 
-// 模拟string 类的实现
+ //模拟string 类的实现
 namespace Mystring
 {
 	class string
@@ -101,7 +102,7 @@ namespace Mystring
 		{
 			m_str = new char[strlen(str) + 1];
 			strcpy(m_str, str);
-			_size = _capacity = strlen(str) + 1;
+			_size = _capacity = strlen(str) ;
 		}
 		string(const string& str) :m_str(nullptr)
 		{
@@ -117,49 +118,43 @@ namespace Mystring
 			}
 			return *this;
 		}
+		~string()
+		{
+			if (m_str)
+			{
+				delete[] m_str;
+				m_str = nullptr; 
+			}
+		}
 	public:
-		char* c_str()
+		const char* c_str()const{return m_str;}
+		iterator begin(){return m_str;}
+		iterator end(){return m_str+_size;}
+		int size(){	return _size;}
+		char& operator [] (int i){return m_str[i];}
+		const char& operator[](size_t i)const
 		{
-			char* newstr = new char[_size];
-			strcpy(newstr, m_str);
-			return newstr;
-		}
-		iterator begin()
-		{
-			return m_str;
-		}
-		iterator end()
-		{
-			return m_str+_size;
-		}
-		int size()
-		{
-			return _size;
-		}
-		char& operator [] (int i)
-		{
+			assert(i < _size &&i >= 0);
 			return m_str[i];
 		}
-
+		int capacity() {return _capacity;}
 	public:
-		string& push_back(char x)
+		void push_back(char x)
 		{
-			if (_size == _capacity)
-			{
-				reverse(_capacity * 2);
-				m_str[size()] = x;
-				_size++;
-			}
-			return *this;
+			if (_size >= _capacity)
+				reserve(_capacity * 2);
+				m_str[_size++] = x;
+				m_str[_size] = '\0';
 		}
-		void reverse(size_t newcap)
+		void reserve(size_t newcap)
 		{
 			if (newcap > _capacity)
 			{
-				char* newp = new char[newcap];
+				char* newp = new char[newcap+1 ];
 				assert(newp != nullptr);
-				_capacity = newcap;
-				strcpy(newp, m_str);
+				_capacity = newcap;  
+				memset(newp, 0, newcap + 1);
+				memcpy(newp, m_str, _size);
 				delete[] m_str;
 				m_str = newp;
 				newp = nullptr;
@@ -170,13 +165,47 @@ namespace Mystring
 			_size = 0;
 			m_str[0] = '\0';
 		}
+		void resize(size_t newSize, char c = '\0')
+		{
+			if (newSize > _size)
+			{
+				if (newSize > _capacity)
+				{
+					reserve(newSize * 2);
+				}
+				memset(m_str + _size, c, newSize - _size);
+			}
+			_size = newSize;
+			m_str[newSize] = '\0';
+		}
+		void append(const char* str)
+		{
+			size_t str_len = strlen(str);
+			if (str_len + _size >= _capacity)
+			{
+				reserve((str_len + _size) * 2);
+			}
+			strcpy(m_str + _size, str);
+		}
+	public:
+		string& operator +=(char c)
+		{
+			 push_back(c);
+			return *this;
+		}
+		bool operator<(const string& s){return (strcmp(m_str, s.m_str) < 0);}
+		bool operator<=(const string& s) { return !(m_str > s.m_str); }
+		bool operator>(const string& s) { return (strcmp(m_str, s.m_str) > 0); }
+		bool operator>=(const string& s) { return !(m_str < s.m_str); }
+		bool operator==(const string& s) { return (m_str ==s.m_str); }
+		bool operator!=(const string& s) { return !(m_str == s.m_str); }
+		
 	protected:
 		static  void _swap(string& s1, string& s2)
 		{
 			swap(s1.m_str, s2.m_str);
 			swap(s1._capacity, s2._capacity);
 			swap(s1._size, s2._size);
-
 		}
 	private:
 		char *m_str;
@@ -188,27 +217,60 @@ namespace Mystring
 		out << str.m_str;
 		return out;
 	}
-
-
 }
 void main()
 {
-	Mystring::string s1 = "hello C";
-	Mystring::string s2(s1);
-	Mystring::string s3;
-	s3 = s2;
-	cout << s2 << endl;
-	cout << s3 << endl;
-	Mystring::string::iterator it = s1.begin();
-	while (it != s1.end())
-	{
-		cout << *it;
-		it++;
-	}
-	cout << endl;
-	cout<<s1.c_str() << endl;
-	cout << endl;
-	s1.push_back('+');
-	s1.push_back('+');
-	cout << s1 << endl;
+	Mystring::string s1("Hello");
+	Mystring::string s2("Linux");
+	if (s1 > s2)
+		cout << "s1 > s2" << endl;
+	else
+		cout << "s1 <= s2" << endl;
+
+
 }
+//void main()
+//{
+//	Mystring::string s1 = "hello C++";
+//	cout << s1 << endl;
+//	cout << "size= " << s1.size() << endl;
+//	cout << "capacity= " << s1.capacity() << endl;
+//	s1.append(" LInux");
+//	cout << s1 << endl;
+//	cout << "size= " << s1.size() << endl;
+//	cout << "capacity= " << s1.capacity() << endl;
+//	s1.reserve(20);
+//	s1.resize(15, '*');
+//	cout << s1 << endl;
+//	cout << "size= " << s1.size() << endl;
+//	cout << "capacity= " << s1.capacity() << endl;
+//	/*s1.push_back('5');
+//	cout << s1 << endl;
+//	cout << "size= " << s1.size() << endl;
+//	cout << "capacity= " << s1.capacity() << endl;
+//	s1 += '5';
+//	cout << s1 << endl;
+//	cout << "size= " << s1.size() << endl;
+//	cout << "capacity= " << s1.capacity() << endl;*/
+//	/*s1.reserve(15);
+//	cout << "size= "<<s1.size() << endl;
+//	cout << "capacity= "<<s1.capacity() << endl;
+//	cout << s1 << endl;*/
+//
+//	/*Mystring::string s2(s1);
+//	Mystring::string s3;
+//	s3 = s2;
+//	cout << s2 << endl;
+//	cout << s3 << endl;
+//	Mystring::string::iterator it = s1.begin();
+//	while (it != s1.end())
+//	{
+//		cout << *it;
+//		it++;
+//	}
+//	cout << endl;
+//	cout<<s1.c_str() << endl;
+//	cout << endl;*/
+//	
+//}
+#endif
