2245063a6ccfa391e4eef07dee3b4ad66417e30a
dzq123321 <2625869004@qq.com>
2020-04-14 23:06:57 +0800

    test

---
+++ test/test/test1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+#include<string>
+using namespace std;
+int main()
+{
+	string s1= "qwer";
+	for (int i = 0; i < s1.size(); i++)
+	{
+		cout<<s1[i]<<" ";
+	}
+	cout << endl;
+}
+//using namespace std;
+//
+//int add(int x=8, int y)
+//{
+//	return x + y;
+//}
+//int main(void)
+//{ 
+//	cout << add(4) << endl;
+//	printf("%s,%10.3s\n", "computer", "computer");
+//	return 0;
+//}
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//#include<limits.h>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<int> v;
+//	for (int i = 0; i < 3 * n; i++)
+//	{
+//		int t;
+//		cin >> t;
+//		v.push_back(t);
+//	}
+//	//≈≈–Ú
+//	sort(v.begin(), v.end());
+//	int size = v.size();
+//	int sum = 0;
+//	int i = size / 3;
+//	while (i < size)
+//	{
+//		sum = sum + v[i];
+//		i += 2;
+//	}
+//	int ret = (sum < 0 ? INT_MAX:sum);
+//	cout << ret << endl;
+//}
