a972e2404b4cce4d775fbb0d53087ff3de402c9c
dzq123321 <2625869004@qq.com>
2020-04-19 10:18:23 +0800

    11

--- test/test/test1.cpp
+++ test/test/test1.cpp
@@ -55,3 +55,34 @@ using namespace std;
 //	int ret = (sum < 0 ? INT_MAX:sum);
 //	cout << ret << endl;
 //}
+//#include<stdio.h>
+//struct S1 {
+//	char c1;    //1+1=2
+//	int i;    //4
+//	char c2;//1+1=2
+//};
+//#define  OffSetOf(s,m)  (&((s*)0)->m)
+//struct A {
+//	int _a : 2;    //_a只占2个比特位
+//	int _b : 5;
+//	int _c : 10;
+//	int _d : 30;
+//};
+//struct S {
+//char a : 3;  
+//char b : 4;  
+//char c : 5;   
+//char d : 4; };
+//int main()
+//{
+//	printf("%d\n", OffSetOf(struct S1, c1));
+//	printf("%d\n", OffSetOf(struct S1, i));
+//	printf("%d\n", OffSetOf(struct S1, c2));
+//	cout <<"sizeof(A)  ="<<sizeof(A) << endl;
+//	cout << "sizeof(S)  =" << sizeof(S) << endl;
+//}
+union Un2 { char c[5];  int i; };
+void main()
+{
+	printf("%d\n", sizeof(union Un2));
+}
